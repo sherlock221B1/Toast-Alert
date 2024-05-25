7084096e736737fc15e76c45d7cbe6d3f4cb5fea
STK <nordvpn4466@gmail.com>
2024-05-25 14:00:26 +0630

    Toast Alert finished

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ const buttonTag = document.querySelector(".button");
 const parentTag = document.querySelector(".parent");
 
 const openTheAlert = ()=>{
+    parentTag.innerHTML="";
     const alertTag = document.createElement("div");
     alertTag.append("Your file was successfully uploaded");
     alertTag.classList.add("alert");
     parentTag.append(alertTag);
-
+    
+    alertTag.style.bottom = `-${alertTag.offsetHeight}px`
+    setTimeout(
+        ()=>{
+            alertTag.style.bottom = "0px"
+        },200
+    )
+    setTimeout(
+        ()=>{
+            alertTag.style.bottom = `-${alertTag.offsetHeight}px` 
+        },2000
+    )
 }
 buttonTag.addEventListener("click", openTheAlert)
